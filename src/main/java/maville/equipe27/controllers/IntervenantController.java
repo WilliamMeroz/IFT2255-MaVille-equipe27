c919796a1be7214a37b07abbf0742b0c858c0875
Ariane Laverrière <146257343+arianelavrr@users.noreply.github.com>
2024-10-03 22:09:14 -0400

    Update IntervenantController.java

--- src/main/java/maville/equipe27/controllers/IntervenantController.java
+++ src/main/java/maville/equipe27/controllers/IntervenantController.java
@@ -8,6 +8,7 @@ import org.beryx.textio.TextIoFactory;
 import org.beryx.textio.TextTerminal;
 
 import static java.lang.System.exit;
+import java.util.concurrent.TimeUnit;
 
 public class IntervenantController implements IController {
 
@@ -45,34 +46,78 @@ public class IntervenantController implements IController {
 
                     case 1:
                         terminal.printf( " \n Voici la liste des travaux en cours : \n " );
+                        while ( reponse == 1 ) {
+                            int revenir = textIO.newIntInputReader()
+                                    .read( " \n 1. Revenir au menu principal \n Enter your choice : " );
+                            if ( revenir != 1 ) {
+                                terminal.printf( " \n Veuillez entrer un numéro valide \n " );
+                            } else {
+                                break;
+                            }
+                        }
                         break;
 
                     case 2:
                         terminal.printf( " \n Recherche : \n " );
+                        while ( reponse == 2 ) {
+                            int revenir = textIO.newIntInputReader()
+                                    .read( " \n 1. Revenir au menu principal \n Enter your choice : " );
+                            if ( revenir != 1 ) {
+                                terminal.printf( " \n Veuillez entrer un numéro valide \n " );
+                            } else {
+                                break;
+                            }
+                        }
                         break;
 
                     case 3:
                         terminal.printf( " \n Voici la liste des requêtes de travail : \n " );
+                        while ( reponse == 3 ) {
+                            int revenir = textIO.newIntInputReader()
+                                    .read( " \n 1. Revenir au menu principal \n Enter your choice : " );
+                            if ( revenir != 1 ) {
+                                terminal.printf( " \n Veuillez entrer un numéro valide \n " );
+                            } else {
+                                break;
+                            }
+                        }
                         break;
 
                     case 4:
                         terminal.printf( " \n Soumettez le nouveau projet ici : \n " );
+                        while ( reponse == 4 ) {
+                            int revenir = textIO.newIntInputReader()
+                                    .read( " \n 1. Revenir au menu principal \n Enter your choice : " );
+                            if ( revenir != 1 ) {
+                                terminal.printf( " \n Veuillez entrer un numéro valide \n " );
+                            } else {
+                                break;
+                            }
+                        }
                         break;
 
                     case 5:
                         terminal.printf( " \n Sélectionnez le chantier à modifier : \n " );
+                        while ( reponse == 5 ) {
+                            int revenir = textIO.newIntInputReader()
+                                    .read( " \n 1. Revenir au menu principal \n Enter your choice : " );
+                            if ( revenir != 1 ) {
+                                terminal.printf( " \n Veuillez entrer un numéro valide \n " );
+                            } else {
+                                break;
+                            }
+                        }
                         break;
 
                     case 6:
                         terminal.printf( " \n Vous vous êtes déconnecté avec succès" );
                         sortir = false;
-                        continue;
-
-                }
-                int revenir = textIO.newIntInputReader()
-                        .read( " \n 1. Revenir au menu principal \n Enter your choice : " );
-                if ( revenir != 1 ) {
-                    terminal.printf( " \n Veuillez entrer un numéro valide \n " );
+                        try {
+                            TimeUnit.SECONDS.sleep(1);
+                            System.exit(0);
+                        } catch(InterruptedException e) {
+                            System.exit(0);
+                        }
                 }
             }
         }
