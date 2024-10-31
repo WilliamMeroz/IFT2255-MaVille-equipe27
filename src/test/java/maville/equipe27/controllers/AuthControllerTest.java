c87bd0f024c80de2df617fc972826756cfd79691
William Méroz-Moreau <merozwilliam@outlook.com>
2024-10-31 09:51:50 -0400

    ajout JUnit dans projet

---
+++ src/test/java/maville/equipe27/controllers/AuthControllerTest.java
@@ -0,0 +1,17 @@
+package maville.equipe27.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+    @Test
+    @DisplayName("Test")
+    void add() {
+        assertEquals(1, 2);
+    }
+
+    @Test
+    void addPropertyChangeListener() {
+    }
+}
