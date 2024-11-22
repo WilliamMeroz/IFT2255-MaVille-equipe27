9212f222651cccd18860e091d9d010825152f1d6
JunYaoXue <jun.yao.xue@umontreal.ca>
2024-11-22 16:54:23 -0500

    Update RequeteTravailStore.java

--- src/main/java/maville/equipe27/helpers/RequeteTravailStore.java
+++ src/main/java/maville/equipe27/helpers/RequeteTravailStore.java
@@ -14,6 +14,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.ArrayList;
 import java.lang.reflect.Type;
 
 public class RequeteTravailStore {
