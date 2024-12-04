d2ba50fa5ca9ed17f191f81641ffb28ae616c754
William Méroz-Moreau <merozwilliam@outlook.com>
2024-12-04 09:16:50 -0500

    ajout forms inscriptoin resident et intervenant

--- src/main/java/maville/equipe27/views/RegisterIntervenantViewControllerFx.java
+++ src/main/java/maville/equipe27/views/RegisterIntervenantViewControllerFx.java
@@ -1,4 +1,107 @@
 package maville.equipe27.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import maville.equipe27.enums.CompanyChoices;
+import maville.equipe27.validators.AuthValidator;
+
+import javax.xml.validation.Validator;
+
 public class RegisterIntervenantViewControllerFx {
+
+    @FXML
+    private Button registerButtonIntervenant;
+
+    @FXML
+    private TextField registerCompanyNameIntervenant;
+
+    @FXML
+    private ComboBox<?> registerCompanyTypeIntervenant;
+
+    @FXML
+    private TextField registerEmailIntervenant;
+
+    @FXML
+    private TextField registerFirstNameIntervenant;
+
+    @FXML
+    private TextField registerIdentifierIntervenant;
+
+    @FXML
+    private TextField registerLastNameIntervenant;
+
+    @FXML
+    private TextField registerPasswordIntervenant;
+
+    @FXML
+    public void initialize() {
+        registerButtonIntervenant.setOnAction(event -> handleRegister());
+    }
+
+    private void handleRegister() {
+        String email = registerEmailIntervenant.getText();
+        String password = registerPasswordIntervenant.getText();
+        String firstName = registerFirstNameIntervenant.getText();
+        String lastName = registerLastNameIntervenant.getText();
+        String companyName = registerCompanyNameIntervenant.getText();
+        String identifier = registerIdentifierIntervenant.getText();
+        String companyTypeStr = (String) registerCompanyTypeIntervenant.getSelectionModel().getSelectedItem();
+
+        CompanyChoices companyType;
+        switch (companyTypeStr) {
+            case "Public":
+                companyType = CompanyChoices.PUBLIC;
+                break;
+            case "Privé":
+                companyType = CompanyChoices.PRIVÉ;
+                break;
+            case "Particulier":
+                companyType = CompanyChoices.PARTICULIER;
+                break;
+        }
+
+        if (!AuthValidator.validateEmail(email)) {
+            showAlert("Courriel invalide", "Le courriel est vide ou n'a pas le bon format");
+            registerEmailIntervenant.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+        if (password.length() < 6) {
+            showAlert("Mot de passe invalide", "Mot de passe invalide");
+            registerPasswordIntervenant.setStyle("-fx-border-color: red;");
+        }
+
+        if (firstName.isEmpty()) {
+            showAlert("Prénom invalide", "Le prénom ne peut pas être vide");
+            registerFirstNameIntervenant.setStyle("-fx-border-color: red;");
+        }
+
+        if (lastName.isEmpty()) {
+            showAlert("Nom de famille invalide", "Le nom de famille ne peut pas être vide");
+            registerLastNameIntervenant.setStyle("-fx-border-color: red;");
+        }
+
+        if (companyName.isEmpty()) {
+            showAlert("Nom de compagnie invalide", "Le nom de compagnie ne peut pas être vide");
+            registerCompanyNameIntervenant.setStyle("-fx-border-color: red;");
+        }
+
+        if (identifier.length() != 8) {
+            showAlert("L'identifiant est invalide", "L'identifiant est un nombre d'exactement 8 chiffres");
+            registerIdentifierIntervenant.setStyle("-fx-border-color: red;");
+        }
+
+
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
--- src/main/java/maville/equipe27/views/RegisterResidentViewControllerFx.java
+++ src/main/java/maville/equipe27/views/RegisterResidentViewControllerFx.java
@@ -1,4 +1,142 @@
 package maville.equipe27.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import maville.equipe27.validators.AuthValidator;
+
+import java.time.LocalDate;
+
 public class RegisterResidentViewControllerFx {
+
+    @FXML
+    private Button registerButtonResident;
+
+    @FXML
+    private TextField registerCivicResident;
+
+    @FXML
+    private DatePicker registerDOBResident;
+
+    @FXML
+    private TextField registerEmailResident;
+
+    @FXML
+    private TextField registerFirstNameResident;
+
+    @FXML
+    private TextField registerLastNameResident;
+
+    @FXML
+    private PasswordField registerPasswordResident;
+
+    @FXML
+    private TextField registerPhoneResident;
+
+    @FXML
+    private TextField registerPostalCodeResident;
+
+    @FXML
+    private TextField registerStreetResident;
+
+    @FXML
+    private TextField registerUnitResident;
+
+    @FXML
+    public void initialize() {
+        registerButtonResident.setOnAction(event -> handleRegister());
+    }
+
+    private void handleRegister() {
+        String email = registerEmailResident.getText();
+        String password = registerPasswordResident.getText();
+        String firstName = registerFirstNameResident.getText();
+        String lastName = registerLastNameResident.getText();
+        LocalDate dob = registerDOBResident.getValue();
+        String phone = registerPhoneResident.getText();
+        String civicStr = registerCivicResident.getText();
+        String street = registerStreetResident.getText();
+        String unitStr = registerUnitResident.getText();
+        String postalCode = registerPostalCodeResident.getText();
+
+
+        if (!AuthValidator.validateEmail(email)) {
+            showAlert("Courriel invalide", "Donner un courriel valide.");
+            registerEmailResident.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+        if (password.length() < 6) {
+            showAlert("Mot de passe invalide", "Donner un mot de passe valide");
+            registerPasswordResident.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+        if (firstName.isEmpty()) {
+            showAlert("Prénom invalide", "Prénom est vide");
+            registerFirstNameResident.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+        if (lastName.isEmpty()) {
+            showAlert("Nom de famille invalide", "Nom de famille vide.");
+            registerLastNameResident.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+        if (dob.isAfter(LocalDate.now())) {
+            showAlert("Date de naissance invalide", "Date de naissance invalide (dans le futur)");
+            registerDOBResident.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+        if (!AuthValidator.validatePhone(phone)) {
+            showAlert("Numéro de téléphone invalide", "Numéro de téléphone doit respecter le format (###-###-####)");
+            registerPhoneResident.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+        int civic;
+        try {
+            civic = Integer.parseInt(civicStr);
+            if (civic < 0) throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            showAlert("Numéro civic invalide", "Numéro civic doit être un numéro positif");
+            registerCivicResident.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+        if (street.isEmpty()) {
+            showAlert("Rue invalide", "La rue ne doit pas être vide");
+            registerStreetResident.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+        if (!unitStr.isEmpty()) {
+            int unit;
+            try {
+                unit = Integer.parseInt(unitStr);
+                if (unit < 0) throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                showAlert("Unité invalide.", "Unité (facultatif) doit être un nombre positif");
+                registerUnitResident.setStyle("-fx-border-color: red;");
+                return;
+            }
+        }
+
+        if (!AuthValidator.validatePostalCode(postalCode)) {
+            showAlert("Code postal invalid", "Le code postal doit respecter le format A1B 2C3");
+            registerPostalCodeResident.setStyle("-fx-border-color: red;");
+            return;
+        }
+
+
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
